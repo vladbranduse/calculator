d798abdb4da16bd74fe4d3e21e1b4bff37a3fa0d
vladbranduse <vladbranduse@gmail.com>
2024-12-25 01:09:38 +0200

    Calculator operation function

--- script.js
+++ script.js
@@ -3,21 +3,34 @@ function add(num1, num2) {
         return num1 + num2;
     }
 }
-
 function subtract(num1, num2) {
     if (!isNaN(num1) && !isNaN(num2)) {
         return num1 - num2;
     }
 }
-
 function multiply(num1, num2) {
     if (!isNaN(num1) && !isNaN(num2)) {
         return num1 * num2;
     }
 }
-
 function divide(num1, num2) {
     if (!isNaN(num1) && !isNaN(num2)) {
         return num1 / num2;
+    }
+}
+
+let firstOperand, operator, secondOperand;
+function operate (firstOperand, operator, secondOperand) {
+    let possibleOperands = ["+", "-", "*", "/"];
+    if (!isNaN(firstOperand) && !isNaN(secondOperand) && possibleOperands.includes(operand)) {
+       if (operator === "+") {
+          return add(firstOperand, secondOperand);
+       } else if (operator === "-") {
+          return subtract(firstOperand, secondOperand);
+       } else if (operator === "*") {
+          return multiply(firstOperand, secondOperand);
+       } else {
+          return divide(firstOperand, secondOperand);
+       }
     }
 }
